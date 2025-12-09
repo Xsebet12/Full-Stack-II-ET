7453c7b4aa55e766c8eb1a539ed1a87ff128918f
Sebastian Gabriel Cornejo Salazar <seb.cornejos@duocuc.cl>
2025-12-08 23:00:52 -0300

    Add EntityGraph to findAll method in RepositorioComuna

--- src/main/java/com/SebastianCornejo/Proyecto/Fullstack/repository/RepositorioComuna.java
+++ src/main/java/com/SebastianCornejo/Proyecto/Fullstack/repository/RepositorioComuna.java
@@ -4,8 +4,12 @@ import com.SebastianCornejo.Proyecto.Fullstack.entity.Comuna;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface RepositorioComuna extends JpaRepository<Comuna, Integer> {
     Optional<Comuna> findByNomComunaIgnoreCase(String nomComuna);
+    @EntityGraph(attributePaths = {"region"})
+    List<Comuna> findAll();
+}
 }
